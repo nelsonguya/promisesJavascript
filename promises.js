96c03e627fa0e5c49866c6067be536a1bd276d2a
unknown <nelsonkimani63>
2021-08-12 15:24:27 +0300

    JS promises

--- promises.js
+++ promises.js
@@ -1,26 +1,28 @@
 
 
-const inventory = {
-    sunglasses: 410010,
+let inventory;
+
+inventory = {
+    sunglasses: 41000,
     pants: 1088,
     bags: 1344
   };
-  
-  // Promise 
-  
-  const myExecutor = (resolve, reject) => {
-    if (inventory.sunglasses > 0) {
-      resolve ('Sunglasses order processed.');
-    } else {
-      reject ('That item is sold out.');
-    }
-  };
-  
-  const orderSunglasses = () => {
-    return new 
-  Promise (myExecutor)
-  };
-  
-  let orderPromise = orderSunglasses();
-  console.log(orderPromise);
+
+// Promise 
+
+const myExecutor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve ('Sunglasses order processed.');
+  } else {
+    reject ('That item is sold out.');
+  }
+};
+
+const orderSunglasses = () => {
+  return new 
+Promise (myExecutor)
+};
+
+let orderPromise = orderSunglasses();
+console.log(orderPromise);
   
